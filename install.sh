1af18e3a3273681b26455a6d45ecf4ac6259f277
y4m3 <y4m3@users.noreply.github.com>
2020-05-24 18:38:10 +0900

    add vimrc alias

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
-ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
+ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
 ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
+ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
 touch $HOME/.zsh_local
