703f430979117e4ede90127d8fa40fe25365b9a5
y4m3 <y4m3@users.noreply.github.com>
2021-12-13 00:21:32 +0900

    add dash lines

--- install.sh
+++ install.sh
@@ -3,8 +3,10 @@
 DOTFILES_DIR=${0:a:h}
 
 # install tpm
+echo "${(r:5::=:)} INSTALL TPM ${(r:62::=:)}"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+echo "\n${(r:5::=:)} ADD SYMBOLIC LINK ${(r:56::=:)}"
 # set config files
 ln -svf $DOTFILES_DIR/zsh/zshrc $HOME/.zshrc
 ln -svf $DOTFILES_DIR/vim/vimrc $HOME/.vimrc
@@ -12,3 +14,5 @@ ln -svf $DOTFILES_DIR/tmux/tmux.conf $HOME/.tmux.conf
 
 # add environment-dependent settings
 touch $HOME/.zshrc.local
+
+echo "\n${(r:5::=:)} DONE ${(r:69::=:)}"
