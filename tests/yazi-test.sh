c8509770b90fc609c19621182380594e48047dc3
y4m3 <y4m3@users.noreply.github.com>
2026-01-09 20:23:12 +0900

    test(yazi): add tests for ya binary and keymap config
    Address review feedback:
    - Add ya (plugin manager) executable and version tests
    - Add keymap.toml existence check

--- tests/yazi-test.sh
+++ tests/yazi-test.sh
@@ -17,8 +17,11 @@ esac
 
 assert_executable "yazi" "yazi installed"
 assert_command "yazi --version" "yazi version prints"
+assert_executable "ya" "ya (plugin manager) installed"
+assert_command "ya --version" "ya version prints"
 
-# Test 3: yazi config file exists (deployed by chezmoi)
+# Config files exist (deployed by chezmoi)
 assert_file_exists "$HOME/.config/yazi/yazi.toml" "yazi config file deployed"
+assert_file_exists "$HOME/.config/yazi/keymap.toml" "yazi keymap config deployed"
 
 print_summary
